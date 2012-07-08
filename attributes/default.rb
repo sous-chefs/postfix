6f693dab412278ed07fdbe4a224058033e54e46a
Giannicola Olivadoti <olinicola@gmail.com>
2012-07-08 13:34:36 +0200

    Fixed fqdn and domain in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,8 +16,8 @@
 # limitations under the License.
 
 default['postfix']['mail_type']  = "client"
-default['postfix']['myhostname'] = fqdn
-default['postfix']['mydomain']   = domain
+default['postfix']['myhostname'] = node['fqdn']
+default['postfix']['mydomain']   = node['domain']
 default['postfix']['myorigin']   = "$myhostname"
 default['postfix']['relayhost']  = ""
 default['postfix']['mail_relay_networks']        = "127.0.0.0/8"
