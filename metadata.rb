d560ae593d635e4e90c7eaebdd21e2dae5f2e4a2
Tim Smith <tsmith84@gmail.com>
2016-04-01 11:52:19 -0700

    Remove attributes from metadata

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,6 @@ license 'Apache 2.0'
 description 'Installs and configures postfix for client or outbound relayhost, or to do SASL auth'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.7.0'
-issues_url 'https://github.com/chef-cookbooks/postfix/issues' if respond_to?(:source_url)
-source_url 'https://github.com/chef-cookbooks/postfix' if respond_to?(:issues_url)
 
 recipe 'postfix', 'Installs and configures postfix'
 recipe 'postfix::sasl_auth', 'Set up postfix to auth to a server with sasl'
@@ -21,47 +19,5 @@ recipe 'postfix::server', 'Sets the mail_type attribute to master'
   supports os
 end
 
-attribute 'postfix/main',
-          display_name: 'postfix/main',
-          description: 'Hash of Postfix main.cf attributes',
-          type: 'hash'
-
-attribute 'postfix/aliases',
-          display_name: 'Postfix Aliases',
-          description: "Hash of Postfix aliases mapping a name to a value. Example 'root' => 'operator@example.com'. See aliases man page for details.",
-          type: 'hash'
-
-attribute 'postfix/transports',
-          display_name: 'Postfix Transports',
-          description: "Hash of Postfix transports mapping a destination to a smtp server. Example 'my.domain' => 'smtp:outbound-relay.my.domain'. See transport man page for details.",
-          type: 'hash'
-
-attribute 'postfix/access',
-          display_name: 'Postfix Access Table',
-          description: "Hash of Postfix accesses mapping a pattern to a action.  Example 'domain.tld' => 'OK'.  See access man page for details.",
-          type: 'hash'
-
-attribute 'postfix/mail_type',
-          display_name: 'Postfix Mail Type',
-          description: 'Is this node a client or server?',
-          default: 'client'
-
-attribute 'postfix/smtp_sasl_user_name',
-          display_name: 'Postfix SMTP SASL Username',
-          description: 'User to auth SMTP via SASL',
-          default: ''
-
-attribute 'postfix/smtp_sasl_passwd',
-          display_name: 'Postfix SMTP SASL Password',
-          description: 'Password for smtp_sasl_user_name',
-          default: ''
-
-attribute 'postfix/relayhost_role',
-          display_name: "Postfix Relayhost's role",
-          description: 'String containing the role name',
-          default: 'relayhost'
-
-attribute 'postfix/use_procmail',
-          display_name: 'Postfix Use procmail?',
-          description: 'Whether procmail should be used as the local delivery agent for a server',
-          default: 'no'
+source_url 'https://github.com/chef-cookbooks/postfix' if respond_to?(:issues_url)
+issues_url 'https://github.com/chef-cookbooks/postfix/issues' if respond_to?(:source_url)
