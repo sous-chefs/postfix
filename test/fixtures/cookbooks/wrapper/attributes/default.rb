35adb3026471b1760f964434dc173e23d5712314
Tim Smith <tsmith84@gmail.com>
2016-04-29 21:22:18 -0700

    Update wrapper test

--- test/fixtures/cookbooks/wrapper/attributes/default.rb
+++ test/fixtures/cookbooks/wrapper/attributes/default.rb
@@ -2,3 +2,4 @@ default['postfix']['main']['smtp_sasl_auth_enable'] = 'yes'
 default['postfix']['main']['relayhost'] = 'please'
 default['postfix']['sasl']['smtp_sasl_user_name'] = 'keep'
 default['postfix']['sasl']['smtp_sasl_passwd'] = 'us'
+default['postfix']['sender_canonical_map_entries'] = {}
