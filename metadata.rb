530a191536772b05198bb13a0ccfe9c90f3d4afb
Tim Smith <tsmith@chef.io>
2017-03-21 21:05:37 -0700

    Update Apache license string in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'postfix'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures postfix for client or outbound relayhost, or to do SASL auth'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '5.0.1'
