6d9defeba5014e9888748eea888f7863ff83b1a3
Tim Smith <tsmith@chef.io>
2017-04-05 22:23:03 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/sasl_auth.rb
+++ recipes/sasl_auth.rb
@@ -3,7 +3,7 @@
 # Cookbook:: postfix
 # Recipe:: sasl_auth
 #
-# Copyright:: 2009-2016, Chef Software, Inc.
+# Copyright:: 2009-2017, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
