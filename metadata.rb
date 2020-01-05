a043528218beb7524652f894c2376cb1b49ef234
Tim Smith <tsmith84@gmail.com>
2020-01-05 12:02:55 -0800

    Require Chef 12.15+
    This gives us modern metadata processing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -11,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/postfix'
 issues_url 'https://github.com/chef-cookbooks/postfix/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.15'
